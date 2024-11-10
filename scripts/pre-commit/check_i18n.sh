381123c69b0e11799dfbd9090ccbd13d2ddddb1b
Işık Kaplan <isik.kaplan@outlook.com>
2024-11-11 02:23:05 +0300

    Add back incorrectly removed translations

--- scripts/pre-commit/check_i18n.sh
+++ scripts/pre-commit/check_i18n.sh
@@ -2,23 +2,15 @@
 
 cd liberwiki-frontend || exit 1
 
-hash1=$(find ./src/locales/* -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum)
-npm run collect-i18n
-hash2=$(find ./src/locales/* -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum)
-
-if [ "$hash2" != "$hash1" ]; then
-	echo "Error: You have translations that are not up-to-date. Please run 'npm run collect-i18n' and commit the changes."
-	exit 1
-fi
-
+npm run collect-i18n -- --fail-on-update
 
 cd ../liberwiki || exit 1
 
-hash3=$(find ./locale/**/*.po -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum)
+hash1=$(find ./locale/**/*.po -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum)
 python manage.py makemessages --all --no-obsolete --no-location
-hash4=$(find ./locale/**/*.po -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum)
+hash2=$(find ./locale/**/*.po -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum)
 
-if [ "$hash3" != "$hash4" ]; then
+if [ "$hash1" != "$hash2" ]; then
 	echo "Error: You have translations that are not up-to-date. Please run 'python manage.py makemessages --all --no-obsolete --no-location' and commit the changes."
 	exit 1
 fi
