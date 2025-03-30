c2f6a03652d12117098be3b46a1367d321b5e2d1
Işık Kaplan <isik.kaplan@outlook.com>
2025-03-31 00:35:31 +0300

    Make complete signup card same width as other auth cards

--- liberwiki-frontend/src/app/(app)/profile/(tabless)/layout.tsx
+++ liberwiki-frontend/src/app/(app)/profile/(tabless)/layout.tsx
@@ -21,7 +21,7 @@ export default async function ProfileLayout({ children }: { children: React.Reac
   return (
     <div className="min-h-screen flex flex-col gap-1 items-center justify-center p-4">
       <div className="flex flex-col justify-center items-center mb-36 w-full gap-2">
-        <Card className="w-full max-w-xl">
+        <Card className="w-full max-w-md">
           <CardContent className="p-6 flex flex-col gap-4">{children}</CardContent>
         </Card>
         <LogoutButton className="w-full gap-2" variant="link">
